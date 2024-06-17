fce79084c8342303620d0087ac0c30487d02ad80
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-06-17 16:24:07 -0500

    Change to function component

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
-      </section>
-    );
-  }
-}
+let Results = (props) => {
+  return(
+    <section>
+      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+    </section>
+  );
+};
 
 export default Results;
