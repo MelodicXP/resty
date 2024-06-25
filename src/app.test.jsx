2854a8890a2ba5e39433fe400012905d237afff6
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-06-25 16:17:44 -0500

    Run GET method test

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { describe, it, expect } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App.jsx';
@@ -10,10 +10,22 @@ describe('App', () => {
 
     render(<App />);
 
-    // const urlInput = screen.getByTestId('url-input');
+    const urlInput = screen.getByTestId('url-input');
+    const getInput = screen.getByTestId('get-input');
+    const submitButton = screen.getByTestId('fetch-api-button');
+    const resultsHeaderInfo = screen.getByTestId('results-header');
+    const resultsAndCounter = screen.getByTestId('results');
+
+    let method = 'get';
+    let url = 'https://four01lab03-api-server-prod.onrender.com/author';
+
+    fireEvent.change(urlInput, {target: {value: url}});
+    fireEvent.click(getInput, {target: {value: method}});
+    fireEvent.click(submitButton);
+
+    expect(resultsHeaderInfo).not.toBeNull();
+    expect(resultsAndCounter).not.toBeNull();
 
   });
-
-
 
 });
