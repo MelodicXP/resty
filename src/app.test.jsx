deaa8132ad469fb20ebcf2a43e5a8ee9b5e933fd
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-06-25 16:02:41 -0500

    Run initial test to ensure test configurations function properly

---
+++ src/app.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { describe, it, expect } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App.jsx';
+
+
+describe('App', () => {
+
+  it('should do a get api call', () => {
+
+    render(<App />);
+
+    // const urlInput = screen.getByTestId('url-input');
+
+  });
+
+
+
+});
