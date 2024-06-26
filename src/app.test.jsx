131ea608a9281ba6c3d6177fa881401c8a6d17cf
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-06-26 08:59:54 -0500

    Test post method

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -28,4 +28,39 @@ describe('App', () => {
 
   });
 
+  it('should do a post api call', () => {
+
+    render(<App />);
+
+    const urlInput = screen.getByTestId('url-input');
+    const postInput = screen.getByTestId('post-input');
+    const submitButton = screen.getByTestId('fetch-api-button');
+    const resultsHeaderInfo = screen.getByTestId('results-header');
+    const resultsAndCounter = screen.getByTestId('results');
+    
+    // User inputs
+    let method = 'post';
+    let url = 'https://four01lab03-api-server-prod.onrender.com/author';
+    let body = '{ "name": "Author5","numBooksPublished": 10}';
+    
+    // Input url and click post method radio button
+    fireEvent.change(urlInput, {target: {value: url}});
+    fireEvent.click(postInput, {target: {value: method}});
+    
+    // Body input field appears, check not null
+    const bodyInput = screen.getByTestId('body-input');
+    expect(bodyInput).not.toBeNull();
+    
+    // Input body for post method
+    fireEvent.change(bodyInput, {target: {value: body}});
+
+    // Click submit button
+    fireEvent.click(submitButton);
+
+    // Header and results not null
+    expect(resultsHeaderInfo).not.toBeNull();
+    expect(resultsAndCounter).not.toBeNull();
+
+  });
+
 });
