bf4970140db11517b1f3bc04d4198c64c2ff80a8
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-07-03 16:35:54 -0500

    Allow list items to make rapi requests from history

--- src/Components/History/index.jsx
+++ src/Components/History/index.jsx
@@ -1,18 +1,58 @@
+import { useState } from 'react';
+
 import './History.scss';
 
-let History = (props) => {
+const History = (props) => {
   const { history } = props;
+
+  // Initialize states
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [body, setBody] = useState(''); // Body to be sent in request
+  const [loading, setLoading] = useState(false) // Hold loading status before and after request
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formData = {
+      method: method,
+      url: url,
+      ...(method === 'post' || method === 'put' ? { body: JSON.parse(body) } : {})
+    };
+    await props.handleApiCall(formData);
+    setLoading(false);
+  };
+
+  // Handle selection of history item
+  const handleHistoryClick = (item) => {
+    setUrl(item.url);
+    setMethod(item.method);
+    setBody(JSON.stringify(item.body || ''));
+  };
 
   return (
     <section>
+      <h3>History</h3>
       <pre data-testid="history">
-        <ul>
+        <form className="methods" onSubmit={handleSubmit}>
           {history.map((item, index) => (
-            <div key ={index}>
-              {JSON.stringify(item, null, 2)};
-            </div>
+            <label key ={index}>
+              <input 
+                data-testid={`${item.method}-input`}
+                onChange={() => handleHistoryClick(item)}
+                type="radio" 
+                name="method"
+                value={item.method}
+              />
+              <span id={item.method}>
+                {JSON.stringify(item, null, 2)};
+              </span>
+              <button data-testid="fetch-api-button" type="submit" disabled={loading}>GO!</button>
+            </label>
           ))}
-        </ul>
+        </form>
       </pre>
     </section>
   );
